44ee9ef0080aec6f569652c87366465f8c1694f7
Zerowhizkey <marcus.piri@cmeducations.se>
2022-03-07 14:01:36 +0100

    todo thing

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TodoPiri from './components/TodoPiri.vue';
+</script>
+
+<template>
+  <img alt="Vue logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Hrtgj7o3IfgxkRoZ39bmjcIF4caCEHLfwA&usqp=CAU" />
+  <TodoPiri msg="Piri Todo List" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoPiri.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const newPiri = ref("");
+const todos = ref([]);
+
+function addPiri() {
+  todos.value.push({ text: newPiri.value, done: false });
+  newPiri.value = "";
+}
+
+function removePiri(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="newPiri" @keyup.enter="addPiri" />
+  <button @click="addPiri">Add</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }"> {{ todo.text }}</span>
+      <button @click="removePiri(todo)">x</button>
+    </li>
+  </ul>
+</template>
+              
+<style scoped>
+
+
+
+li {
+  list-style: none;
+  display: flex;
+  margin-bottom: 5px;
+  justify-content: space-between;
+  width: 25%;
+  font-weight: bold;
+  font-size: 20px;
+  border: solid 1px;
+}
+  
+
+.done {
+  text-decoration: line-through;
+  opacity: 0.5;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+ flex-direction: column;
+ align-items: center;
+ margin-right: 45px;
+  
+}
+
+
+</style>
